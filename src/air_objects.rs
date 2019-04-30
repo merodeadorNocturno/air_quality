330a2eb44d1ee6dfb724097e27e5833f36f24947
tonamartinez <tonatiuh.martinez@globant.com>
2019-04-30 11:16:52 -0500

    Added a module that contains the air_data structs.

---
+++ src/air_objects.rs
@@ -0,0 +1,46 @@
+pub mod air_data {
+  use serde::Deserialize;
+
+  #[derive(Deserialize)]
+  pub struct AirData {
+    pub sid: String,
+    pub id: String,
+    pub position: u16,
+    pub created_at: u32,
+    pub created_meta: serde_json::Value,
+    pub updated_at: u32,
+    pub updated_meta: serde_json::Value,
+    pub meta: String,
+    pub measure_id: String,
+    pub measure_name: String,
+    pub measure_type: String,
+    pub stratification_level: String,
+    pub state_fips: String,
+    pub state_name: String,
+    pub county_fips: String,
+    pub county_name: String,
+    pub report_year: String,
+    pub value: String,
+    pub unit: String,
+    pub unit_name: String,
+    pub data_origin: String,
+    pub monitor_only: String,
+  }
+
+  pub struct UsefulData {
+    pub measure_id: u16,
+    pub measure_name: String,
+    pub measure_type: String,
+    pub stratification_level: String,
+    pub state_fips: u16,
+    pub state_name: String,
+    pub county_fips: u16,
+    pub county_name: String,
+    pub report_year: u16,
+    pub value: f64,
+    pub unit: String,
+    pub unit_name: String,
+    pub data_origin: String,
+    pub monitor_only: u8,
+  }
+}
