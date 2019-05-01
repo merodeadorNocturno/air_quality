9f77df02c8e627b0d11745b4017c1bd085ba6ee1
tonamartinez <tonatiuh.martinez@globant.com>
2019-05-01 14:59:53 -0500

    Removed unnecessary parsing filter.

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,48 +1,92 @@
-console.time('nodeStatic:');
+console.time('Node:');
 const airQuality = require('../json_files/airQuality.json');
 
-const reducer = (data) => {
+const reducer = data => {
     const redux = (accumulator, currentValue) => accumulator + currentValue.value;
     const sum_value = data.reduce(redux, 0);
     return sum_value / data.length;
 }
 
-const straightData = (data) => {
-
-    // const countyName = 'El Paso';
-    // const reportYear = 2012;
-    const measureId = 292;
+const createDataObject = data => {
 
     const myData = data
-    .map(col => col
-        .filter((item, index) => index > 7)
-    )
     .map(item => ({
-        measure_id: parseInt(item[0]),
-        measure_name: item[1],
-        measure_type: item[2],
-        stratification_level: item[3],
-        state_fips: parseInt(item[4]),
-        state_name: item[5],
-        county_fips: parseInt(item[6]),
-        county_name: item[7],
-        report_year: parseInt(item[8]),
-        value: parseFloat(item[9]),
-        unit: item[10],
-        unit_name: item[11],
-        data_origin: item[12],
-        monitor_only: parseInt(item[13]),
+        measure_id: parseInt(item[8]),
+        measure_name: item[9],
+        measure_type: item[10],
+        stratification_level: item[11],
+        state_fips: parseInt(item[12]),
+        state_name: item[13],
+        county_fips: parseInt(item[14]),
+        county_name: item[15],
+        report_year: parseInt(item[16]),
+        value: parseFloat(item[17]),
+        unit: item[18],
+        unit_name: item[19],
+        data_origin: item[20],
+        monitor_only: parseInt(item[21]),
     }))
-    // .filter(item => item.value > 1 && item.measure_id === measureId)
-    .filter(item => item.measure_id === measureId)
-
-    console.log(`Average: ${reducer(myData)}`);
 
     return myData;
 };
 
-const dataArray = straightData(airQuality.data);
-console.timeEnd('nodeStatic:');
-console.log(dataArray.length);
+const averageData = data => {
+    const avgData = reducer(data);
+    return avgData;
+};
 
-// console.log(dataArray[0].measure_id);
+const getMeasureTypes = data => {
+    const myTypesArray = data
+        .map(item => item.measure_type);
+
+    return [...new Set(myTypesArray)];
+};
+
+const getMeasureIds = data => {
+    const myArray = data
+        .map(item => item.measure_id);
+
+    return [...new Set(myArray)];
+};
+
+const filterByType = (data, type) => {
+    return data
+        .filter(item => item.measure_type === type);
+};
+
+const data = createDataObject(airQuality.data);
+const myMeasureIds = getMeasureIds(data);
+const myTypes = getMeasureTypes(data);
+
+const counts = filterByType(data, 'Counts');
+const avgCounts = averageData(counts);
+
+const percent = filterByType(data, 'Percent');
+const avgPct = averageData(percent);
+
+const average = filterByType(data, 'Average');
+const avgAvg = averageData(average);
+
+
+console.timeEnd('Node:');
+
+console.log(`measure_ids ${myMeasureIds}`);
+console.log(`measure_types ${myTypes}`);
+console.group();
+console.log(` `);
+console.log(`Total Counts registers ${counts.length}`);
+console.log(`Average in Counts ${avgCounts}`);
+console.log(` `);
+console.groupEnd();
+console.group();
+console.log(` `);
+console.log(`Total Percent registers ${percent.length}`);
+console.log(`Average in Percent ${avgPct}`);
+console.log(` `);
+console.groupEnd();
+console.group();
+console.log(` `);
+console.log(`Total Average registers ${average.length}`);
+console.log(`Average in Average ${avgAvg}`);
+console.log(` `);
+console.groupEnd();
