7de7e7933023c40cfeea052fc8825499491524a1
tonatiuhmartinez <tonatiuh.martinez@globant.com>
2019-11-11 21:48:04 -0600

    moved get_dir_path fn to module

--- src/main.rs
+++ src/main.rs
@@ -113,38 +113,6 @@ fn filter_by_type(data: &Vec<UsefulData>, my_type: String) -> Vec<UsefulData> {
   my_reference
 }
 
-fn get_dir_path() -> PathBuf {
-  let option_home_dir = dirs::home_dir();
-  match option_home_dir {
-    Some(mut dir) => {
-      dir.push("Development");
-      dir.push("rust");
-      dir.push("air_quality");
-      dir.push("json_files");
-      dir.push("json_files");
-      dir.set_file_name("airQuality");
-      dir.set_extension("json");
-
-      dir
-    },
-    None => {
-      let mut path: PathBuf = [
-        r"/",
-        "home",
-        "tonatiuh.martinez",
-        "Development",
-        "rust",
-        "air_quality",
-      ].iter().collect();
-      path.push("json_files");
-      path.set_file_name("airQuality");
-      path.set_extension("json");
-
-      path
-    }
-  }
-}
-
 fn main() {
   let init = Instant::now();
   let filepath = get_dir_path();
